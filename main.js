370f37b39ef548efbbcc95206e46e721e4f1bcfa
YiJio <zhiyuzhengju@gmail.com>
2023-01-14 19:58:58 -0800

    Complete change
    This commit should have been made in February, just committing it for the purpose of relooking back changes.

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 // variables
 const body = document.getElementsByTagName("body");
+var refresh = document.getElementById("refresh");
+var mepic = document.getElementById("mepic");
+var dserv = document.getElementById("services");
+if(dserv) { var dservb = dserv.getElementsByClassName("item"); }
+var posts = document.getElementById("posts");
+if(posts) { var pitem = posts.getElementsByClassName("item"); }
+const card = document.querySelector(".posts .item");
 const button = document.querySelector(".menubutton");
 const menu = document.querySelector(".menu");
 const nav = document.querySelector(".nav");
@@ -24,3 +31,77 @@ function toggleMenu() {
 		showMenu = false;
 	}
 }
+
+// refresh to switch profile picture
+if(refresh) {
+	refresh.addEventListener("click", function() {
+		if(mepic.src.includes('8')) { mepic.src = '../images/profile/profile4.png'; }
+		else { mepic.src = '../images/profile/profile8.png'; }
+	});
+}
+
+/*card.onmousemove = function (e) {
+  const x = e.pageX - card.offsetLeft;
+  const y = e.pageY - card.offsetTop;
+
+  card.style.setProperty( '--x', x + 'px' );
+  card.style.setProperty( '--y', y + 'px' );
+}*/
+/*if(posts) {
+	for(var i = 0; i < pitem.length; i++) {
+		pitem[i].onmouseover = function(e) { this.style.zIndex = '9'; }
+		pitem[i].onmouseout = function(e) { this.style.zIndex = '1'; }
+	}
+}*/
+
+// bind link to main boxes
+if(dserv) {
+	for(var i = 0; i < dservb.length; i++) {
+		dservb[i].addEventListener("click", function() {
+			/*var id = this.id;
+			if(id == 'dev') { window.location.href = 'develop.html'; }
+			else if(id == 'des') { window.location.href = 'design.html'; }
+			else if(id == 'dra') { window.location.href = 'draw.html'; }*/
+			window.location.href = 'work.html';
+		});
+	}
+}
+
+function reveal() {
+  var reveals = document.querySelectorAll(".lang .reveal");
+  var reveals2 = document.querySelectorAll(".soft .reveal");
+  var reveals3 = document.querySelectorAll("#exp .reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("fade-left");
+    }
+  }
+  for (var i = 0; i < reveals2.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals2[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals2[i].classList.add("animate-in");
+    }
+  }
+  for (var i = 0; i < reveals3.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals3[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals3[i].classList.add("animate-in");
+    }
+  }
+}
+
+$(".box .pc").each(function() {
+	$(this).animate({
+		width: $(this).html(),
+    }, 1500);
+});
+
+window.addEventListener("scroll", reveal);
